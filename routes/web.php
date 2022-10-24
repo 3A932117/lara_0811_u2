c48be30cc05cc75ec4cbd6570c9a15ef4ae86ca1
Huang Jia Yi <3a932117@gm.student.ncut.edu.tw>
2022-10-24 20:26:41 +0800

    產生Controller、將Route的內容搬至Controller內

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -57,3 +58,5 @@ Route::group(['prefix' => 'admin'], function() {
         return 'admin dashboard';
     });
 });
+
+Route::get('home',[HomeController::class, 'index'])->name('home.index');
