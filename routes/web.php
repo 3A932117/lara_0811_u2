b54fcfdb8752f6f33261fe539b2a164b107e7f3c
Huang Jia Yi <3a932117@gm.student.ncut.edu.tw>
2022-10-24 20:03:15 +0800

    為Route命名，增加連結

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,12 @@ Route::get('/', function () {
     return 'welcome';
 });
 
-//設定Route跳轉頁面
+//設定Route跳轉路由
 Route::get('r1', function () {
     return redirect('r2');
+});
+Route::get('r2', function() {
+    return view('welcome');
 });
 
 //修改Route接受參數
@@ -36,9 +39,9 @@ Route::get('r1', function () {
 //修改參數成選擇性
 Route::get('hello/{name?}', function($name='Everybody') {
     return 'Hello, '.$name;
-});
+})->name('hello.index');//為Route取名為hello.index
 
 //隨便增加路由
-Route::get('nothing', function(){
+/*Route::get('nothing', function(){
     return 'nothing';
-});
+});*/
